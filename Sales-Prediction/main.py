6414903f542f94cc9abf43940edf7c462e8b346d
PaulOrtizCrittenden <paulortizcrittenden@westernwyoming.edu>
2025-02-23 11:02:13 -0700

    Update main.py

--- Sales-Prediction/main.py
+++ Sales-Prediction/main.py
@@ -1,6 +1,6 @@
 #
-# Name
-# Date
+# Paul Ortiz
+# 2/15/2025
 # Sales Prediction Programming Project
 # COSC 1010
 #
@@ -12,3 +12,12 @@
 # Calculate the projected profit.
 
 # Print the projected profit.
+
+# get the projexted total sales
+total_sales = float( input('enter the projected sales: ') )
+
+# calculate profit as 23% of total sales.
+profit = total_sales * 0.23
+
+#display profit.
+print('The profit is $', format(profit, ',.2f'))
