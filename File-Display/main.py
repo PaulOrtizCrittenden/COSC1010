e82b8686164ad20170a73b482aad447be876e8c8
PaulOrtizCrittenden <paulortizcrittenden@westernwyoming.edu>
2025-04-05 19:01:47 -0600

    Update main.py

--- File-Display/main.py
+++ File-Display/main.py
@@ -1,7 +1,23 @@
 #
-# Name
-# Date
-# File Display Programming Project
-# COSC 1010
+# Open the file
+# For each line in the file:
+#   Read the line and display it
+# Close the file
 #
-# Use comments liberally throughout the program. 
+# step 1: open the file
+
+myFile = open('numbers.txt', 'r')
+
+# read and display the file's contents
+
+for line in myFile:
+    number = int(line)
+    print(number)
+
+# close the file
+
+myFile.close()
+
+#ready to test
+# Test success
+#ready to grade
