46b841e51fc5b310142a6521ffecd7f44335b86c
PaulOrtizCrittenden <paulortizcrittenden@westernwyoming.edu>
2025-04-05 19:31:17 -0600

    Update main.py

--- Average-of-Numbers/main.py
+++ Average-of-Numbers/main.py
@@ -1,7 +1,42 @@
 #
-# Name
-# Date
+# Paul Ortiz
+# 4/5/2025
 # Average of Numbers Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+#
+# Open the file
+
+myfile = open('numbers.txt', 'r')
+
+# We're going to need a total, and then a count of numbers
+
+total = 0
+
+count = 0
+
+# read and process
+
+for line in myfile:
+    number = int(line)
+
+# convert lines to integers
+    total += number
+
+# get a total count
+    count += 1
+
+#calculate and display the average 
+if count > 0:
+    average = total / count
+    print(int(average)) #first go around I didn't have the average print as an integer, which felt weird because i converted everything else as an integer.
+else:
+    print('This file is empty')
+
+# close the file
+
+myfile.close()
+
+# ready to test
+# test success, ready to grade
