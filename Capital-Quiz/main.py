e45ea69a00b740dbca1d0d4ac63c0a8be4eff7c2
PaulOrtizCrittenden <paulortizcrittenden@westernwyoming.edu>
2025-04-23 17:17:26 -0600

    Update main.py

--- Capital-Quiz/main.py
+++ Capital-Quiz/main.py
@@ -1,6 +1,6 @@
 #
-# Name
-# Date
+# Paul Ortiz
+# 4/23/2025
 # Capital Quiz Programming Project
 # COSC 1010
 #
@@ -8,8 +8,43 @@
 
 import random
 
+#constant for the number of states to quiz the user on
+NUM_STATES = 50
+
 def main():
+    #initialize the state_caps dictionary
+    state_caps = state_cap_dictionary()
+
+    #intial varibales to keep count of the answers
+    correct = 0
+    incorrect = 0
+
+    #quiz the user
+    for count in range(NUM_STATES):
+        #get a random entry from the dictionary.
+        state, capital = state_caps.popitem()
+
+        #quiz the user
+        print('What is the capital of ', state, '? ', end=' ')
+        response = input()
+
+        # is the answer correct?
+        if response.lower() == capital.lower():
+            correct += 1
+            print('Correct!')
+        
+        else:
+            incorrect += 1
+            print('Incorrect.')
+    
+    # Display the results
+    print('Correct responses: ', correct)
+    print('Incorrect repsonses: ', incorrect)
+
     # Initialize dictionary
+    
+def state_cap_dictionary():
+    
     capitals = {'Alabama':'Montgomery', 'Alaska':'Juneau',
                 'Arizona':'Phoenix', 'Arkansas':'Little Rock',
                 'California':'Sacramento', 'Colorado':'Denver',
@@ -35,9 +70,11 @@ def main():
                 'Vermont':'Montpelier', 'Virginia':'Richmond',
                 'Washington':'Olympia', 'West Virginia':'Charleston',
                 'Wisconsin':'Madison', 'Wyoming':'Cheyenne'}
+    return capitals
 
     # Local variables
 
+ 
     # Continue until user quits the game.
 
 # Call the main function.
